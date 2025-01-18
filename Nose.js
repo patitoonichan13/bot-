44c75e06ac37849304cd840fd56c3b4d4bedf8dc
LuLuBot <patitoonichan130424@gmail.com>
2025-01-17 23:26:41 -0500

    Update and rename README.md to Node.js

---
+++ Nose.js
@@ -0,0 +1,9 @@
+# Crear el directorio del proyecto
+mkdir whatsapp-antilink-bot
+cd whatsapp-antilink-bot
+
+# Inicializar el proyecto Node.js
+npm init -y
+
+# Instalar las dependencias necesarias
+npm install whatsapp-web.js qrcode-terminal
